110c9f9ddc7c6ddc4ffa179444c63b41b4b6f7ae
Sai Surya Aari <surya.5.arri@gmail.com>
2024-11-11 08:45:25 -0500

    Intial Commit to the Git

---
+++ etl_project/config/dev_config.py
@@ -0,0 +1,9 @@
+config_dev = {
+        "data_source": "https://data.cityofnewyork.us/resource/t29m-gskq.json",
+        "email_sender": "surya.5.aari@gmail.com",
+        "email_reciver":"surya.5.aari@gmail.com",
+        "file_storage":"",
+        "postgres_connection_string": "jdbc:postgresql://localhost:5432/taxi",
+        "db_user": "postgres",
+        "db_pwd": "pencil#7"
+}
